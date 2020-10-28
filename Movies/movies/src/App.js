1c6646749515e4d8c63dc922934268960139c63b
Addam Reis <addamreis@gmail.com>
2020-10-27 22:34:26 -0300

    nais/Movies (master)

---
+++ Movies/movies/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style/style.css';
+import Routes from './Routes';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app padrao">
+                <Routes />
+            </div>
+        );
+    }
+}
+
+export default App;
